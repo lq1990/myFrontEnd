4a5fbbfa0c3a8663a7b1971d0c795f7fdf04ac27
Qiang <chinalq90@gmail.com>
2018-12-03 23:22:36 +0100

    vue: component, slot, component - event, com-lifecycle, SFC, ElelemtUI

---
+++ Vue-file/VueStart/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+      <hr>
+      <div class="block">
+        <span class="demonstration">默认</span>
+        <el-slider v-model="value1"></el-slider>
+      </div>
+    </div>
+    <hr>
+    <demo></demo>
+  </div>
+</template>
+
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<script>
+import "element-ui/lib/theme-chalk/index.css";
+import Demo from "./components/Demo";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      age: 19
+    };
+  },
+  components: {
+    demo: Demo
+  }
+};
+</script>
---
+++ Vue-file/VueStart/src/plugins/element.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+import { Button, Slider } from "element-ui";
+
+Vue.use(Button); // use: 就是把Button注册为全局主件
+Vue.use(Slider);
